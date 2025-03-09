20bbf7ac2019e3288452630e01e5896405a4ab05
Milkslayer <muhammed.bakijev@gmail.com>
2025-03-09 23:02:47 +0100

    add intial animated content

---
+++ src/lib/components/LoadingBar.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	type Props = {
+		value?: number;
+		class?: string;
+	};
+
+	let { value, class: className }: Props = $props();
+
+	const getValue = () => {
+		if (!value) return 0;
+		if (value < 0) return 0;
+		if (value > 100) return 100;
+		return value;
+	};
+</script>
+
+<div
+	class="animate__animatedd animate__fadeIn animate__delay-5s h-2 rounded-2xl border border-stone-400 bg-white"
+>
+	<div class={`h-full bg-slate-600 ${className ? className : ''}`} style={`width: ${value}%`}></div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,34 @@
+<script lang="ts">
+	import LoadingBar from '$lib/components/LoadingBar.svelte';
+	import { onMount } from 'svelte';
+
+	const h1Text = 'Take a break,';
+	const h2Text = 'enjoy the view.';
+	const typingSpeed = 50;
+	let p1: HTMLElement;
+	let p2: HTMLElement;
+	let i = 0;
+
+	function sleep(ms: number) {
+		return new Promise((resolve) => setTimeout(resolve, ms));
+	}
+
+	const fill = async (header: HTMLElement, text: string) => {
+		for (let i = 0; i < text.length; i++) {
+			header.innerHTML += text.charAt(i);
+			await sleep(50);
+		}
+	};
+
+	const fillP1 = () => fill(p1, h1Text);
+	const fillP2 = () => fill(p2, h2Text);
+
+	onMount(() => {
+		setTimeout(fillP1, 1800);
+		setTimeout(fillP2, 2800);
+	});
+</script>
+
 <div class="relative flex h-dvh flex-col items-center justify-center overflow-hidden">
 	<div class="-z-30 hidden md:block">
 		<img
@@ -5,7 +36,7 @@
 			alt="blurred bg"
 			class="absolute top-1/2 left-0 -z-20 aspect-9/7 w-full -translate-y-1/2"
 		/>
-		<div class="absolute top-0 left-0 h-full w-full blur"></div>
+		<div class="absolute top-0 left-0 h-full w-full blur backdrop-blur-lg"></div>
 	</div>
 
 	<img
@@ -14,9 +45,14 @@
 		class="absolute top-0 left-0 -z-10 aspect-9/7 h-dvh w-auto object-cover md:left-1/2 md:-translate-x-1/2"
 	/>
 
-	<div class="ffont rounded-lg bg-stone-200/60 p-8 text-center text-4xl">
-		<h1>Take a break</h1>
-		<h2>Enjoy the view</h2>
+	<div class="flex flex-col gap-2">
+		<div
+			class="ffont animate__animated animate__fadeInUp animate__slow rounded-lg bg-stone-200/60 p-8 text-center text-4xl"
+		>
+			<p bind:this={p1}></p>
+			<p bind:this={p2}></p>
+		</div>
+		<LoadingBar value={1} />
 	</div>
 
 	<div
