05e16f10cfd842c999b33b3141707bfd5fddc848
Milkslayer <muhammed.bakijev@gmail.com>
2025-03-09 20:15:50 +0100

    initial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<div class="relative flex h-dvh flex-col items-center justify-center overflow-hidden">
+	<div class="-z-30 hidden md:block">
+		<img
+			src="img/small.jpg"
+			alt="blurred bg"
+			class="absolute top-1/2 left-0 -z-20 aspect-9/7 w-full -translate-y-1/2"
+		/>
+		<div class="absolute top-0 left-0 h-full w-full blur"></div>
+	</div>
+
+	<img
+		src="img/small.jpg"
+		alt="bg"
+		class="absolute top-0 left-0 -z-10 aspect-9/7 h-dvh w-auto object-cover md:left-1/2 md:-translate-x-1/2"
+	/>
+
+	<div class="ffont rounded-lg bg-stone-200/60 p-8 text-center text-4xl">
+		<h1>Take a break</h1>
+		<h2>Enjoy the view</h2>
+	</div>
+
+	<div
+		class="ffont absolute right-2 bottom-1 flex flex-row items-center justify-center rounded-lg bg-sky-700 px-1 py-0.5 text-2xl font-bold text-white italic"
+	>
+		<a class="text-base" href="https://www.linkedin.com/in/muhammed-bakijev/">MB</a>
+	</div>
+</div>
+
+<style>
+	.blur {
+		backdrop-filter: blur(16px);
+	}
+</style>
